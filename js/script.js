24b57c8d53a4c7cc7496a747801cfd2d4a16cd19
clarelimo <clare.limo@student.moringaschool.com>
2021-06-12 12:49:21 +0300

    got the elemnts entered by the user

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var male = [{day:"Sunday",name:"Kwasi"},{day:"Monday",name:"Kwadwo"},{day:"Tuesday",name:"Kwabena"},{day:"Wednesday",name:"Kwaku"},{day:"Thursday",name:"Yaw"},{day:"Friday",name:"Koffi"},{day:"Saturday",name:"Kwame"}];
 var female = [{day:"Sunday",name:"Akosua"},{day:"Monday",name:"Adwoa"},{day:"Tuesday",name:"Abenaa"},{day:"Wednesday",name:"Akua"},{day:"Thursday",name:"Yaa"},{day:"Friday",name:"Afua"},{day:"Saturday",name:"Ama"}];
 
+var dateOfBirth = document.getElementById("dob").value;
+var isFemale = document.getElementById("gridRadios1").value;
+var isMale = document.getElementById("gridRadios2").value;
+
 
 
  
