a6e4698599c3b1275d46344d05398f57b9274526
clarelimo <clare.limo@student.moringaschool.com>
2021-06-12 12:51:30 +0300

    wrote a fuction to calculate the day week

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ var year = birthday[2];
 var cc = year.slice(0,2); // century digit
 var yy = year.slice(2,4); // year digit
 
+function dayOfWeek(){
+    if(dd <= 0 || dd > 31) alert("You have entered an invalid day");
+    if(mm <= 0 || mm > 12) alert("You have entered an invalid month");
 
+    var day =  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+}
 
  
