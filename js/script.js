b87701be491c2eb3c2157e3126d98e434fdb9f05
clarelimo <clare.limo@student.moringaschool.com>
2021-06-14 00:37:52 +0300

    convert string to int

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ var male = [{day:"Sunday",name:"Kwasi"},{day:"Monday",name:"Kwadwo"},{day:"Tuesd
 var female = [{day:"Sunday",name:"Akosua"},{day:"Monday",name:"Adwoa"},{day:"Tuesday",name:"Abenaa"},{day:"Wednesday",name:"Akua"},{day:"Thursday",name:"Yaa"},{day:"Friday",name:"Afua"},{day:"Saturday",name:"Ama"}];
 
 function dayOfWeek(){
-    var dd = document.getElementById("date").value;
-    var mm = document.getElementById("month").value;
+    var dd = parseInt(document.getElementById("date").value);
+    var mm = parseInt(document.getElementById("month").value);
     var year = document.getElementById("year").value;
-
-    var cc = year.slice(0,2); // century digit
-    var yy = year.slice(2,4); // year digit
+   
+    var cc = parseInt(year.slice(0,2)); // century digit
+    var yy = parseInt(year.slice(2,4)); // year digit
 
     if(dd <= 0 || dd > 31) alert("You have entered an invalid day");
     if(mm <= 0 || mm > 12) alert("You have entered an invalid month");
